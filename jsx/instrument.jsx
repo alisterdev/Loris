1b933ddebd1893ce227ee2181da5b99abddae65d
Xavier <xavierlb.mavan@gmail.com>
2016-05-13 11:45:42 -0400

    almost working page

---
+++ jsx/instrument.jsx
@@ -0,0 +1,60 @@
+LorisInstrument = React.createClass({
+    propTypes: {
+        name : React.PropTypes.string.isRequired
+    },
+
+    getInitialState: function() {
+        return null;
+    },
+
+    getDefaultProps: function() {
+        return ({
+            "Meta" : {
+                "InstrumentVersion": "BMI v0.0.1-dev",
+                "InstrumentFormatVersion" : "v0.0.1-dev",
+                "ShortName" : "BMI",
+                "LongName"  : "Body Mass Index",
+                "IncludeMetaDataFields" : false
+            },
+            "Elements" : [
+                {
+                    "Type": "numeric",
+                    "Name": "height",
+                    "Description": "Your height (inches)",
+                    "Options" : {
+                        "NumberType": "integer",
+                        "MinValue": 0,
+                        "MaxValue": 100
+                    }
+                },
+                {
+                    "Type": "numeric",
+                    "Name": "weight",
+                    "Description": "Your weight (lbs)",
+                    "Options" : {
+                        "NumberType": "integer",
+                        "MinValue": 0,
+                        "MaxValue": 500
+                    }
+                },
+                {
+                    "Type": "score",
+                    "Name": "bmi",
+                    "Description": "Your BMI",
+                    "Options": {
+                        /* None currently */
+                    }
+                }
+            ]
+        });
+    },
+
+    render: function () {
+        return (
+            <div>
+                <h1>{this.props.Meta.LongName}</h1>
+            </div>
+        );
+    }
+});
+
---
+++ modules/JSON_instrument_dev/templates/menu_JSON_instrument_dev.tpl
@@ -0,0 +1,2 @@
+
+<div id="instrument-container"></div>
